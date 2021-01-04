1ad195ad15372b1ef8c167abf136e2b05044da24
louisedgar <louisedgar@ymail.com>
2021-01-04 11:51:40 +0700

    layout 2.0

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="max-w-5xl text-gray-800 text-lg mx-auto">
+            <ul className="flex flex-row-reverse px-6 pt-10">
+                <li className="mr-16">Contact</li>
+                <li className="mr-16">Work</li>
+            </ul>
+        </div>
+    )
+}
